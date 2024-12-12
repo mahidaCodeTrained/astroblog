7d47bbdd98293f74464309721a1561a64a8431d7
Abhishek Mahida <abhishekmahida56@gmail.com>
2024-12-12 15:13:59 +0000

    testing keys

--- astroblog/settings.py
+++ astroblog/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x10co)h*#p8x2x6_txvv_$9q^1yggrj!5%$wpjia+ix%kl7kbr'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
