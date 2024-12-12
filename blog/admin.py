4b49653b51d7a6b8844aeccd5bc2e37b8834fca6
Abhishek Mahida <abhishekmahida56@gmail.com>
2024-12-12 23:31:18 +0000

    import new model into admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import PostBlog
+from .models import Comment
 # Register your models here.
 admin.site.register(PostBlog)
+admin.site.register(Comment)
