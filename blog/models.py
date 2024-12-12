bb3dc0a030ac0c90acf05d3250e79d6ba11aac10
Abhishek Mahida <abhishekmahida56@gmail.com>
2024-12-12 16:19:29 +0000

    update PostBlog model

--- blog/models.py
+++ blog/models.py
@@ -17,3 +17,4 @@ class PostBlog(models.Model):
     excerpt = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    updated_on = models.DateTimeField(auto_now=True)
