3daae762726d52efb38a2f0ff430d27c543d976d
Abhishek Mahida <abhishekmahida56@gmail.com>
2024-12-12 15:39:34 +0000

    import cloudinary for images.

--- astroblog/settings.py
+++ astroblog/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-mahidacodetra-astroblog-b7anplv6oaj.ws.codeinstitute-ide.net',
     '.herokuapp.com'
@@ -43,6 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Cloudinary
+    'cloudinary_storage',
+    'cloudinary',
     'blog',
     'about',
 ]
