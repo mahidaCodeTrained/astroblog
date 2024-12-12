8b38bd6e60c67ecc80b14216d44278e85afe5833
Abhishek Mahida <abhishekmahida56@gmail.com>
2024-12-12 14:41:27 +0000

    update urls.py add url route for blogger view

--- astroblog/urls.py
+++ astroblog/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from blog import views as index_views
+from blog import views as blogger_views
 
 urlpatterns = [
-    path('', index_views.index, name='index'),
+    path('blogger/', blogger_views.blogger, name='blogger'),
     path('admin/', admin.site.urls),
 ]
