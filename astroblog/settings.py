3f76a05a62b814d4894a1eef98b27d7e1fc0823f
Abhishek Mahida <abhishekmahida56@gmail.com>
2024-12-13 01:32:20 +0000

    append settings to ensure base template is read.

--- astroblog/settings.py
+++ astroblog/settings.py
@@ -21,6 +21,7 @@ if os.path.isfile('env.py'):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -30,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-mahidacodetra-astroblog-b7anplv6oaj.ws.codeinstitute-ide.net',
     '.herokuapp.com'
@@ -68,7 +69,7 @@ ROOT_URLCONF = 'astroblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
