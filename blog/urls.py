75c61311ac3dd2823aca7a046c12c27caa1fe286
Abhishek Mahida <abhishekmahida56@gmail.com>
2024-12-13 23:10:20 +0000

    create url path for detailed_posts template

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.FetchBlog.as_view(), name='blog-home'),  
+    path('', views.FetchBlog.as_view(), name='blog-home'),
+    path('<slug:slug>/', views.detailed_posts, name='detailed_posts'),
 ]
