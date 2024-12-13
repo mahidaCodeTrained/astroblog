877e04d3fbac6757629ac8a2e4d2002793958e8b
Abhishek Mahida <abhishekmahida56@gmail.com>
2024-12-13 15:42:25 +0000

    update index.html and views.py to include paginator

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,20 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.views import generic
 from .models import PostBlog
 
 def fetchblog(request):
-    # Fetch posts that are not drafts i'll use function based viewing instead of class based.
-    posts = PostBlog.objects.filter(status=1)  
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    # Fetch posts that are not drafts
+    posts = PostBlog.objects.filter(status=1)
+    
+    # Set pagination: 6 posts per page (simulate paginate_by)
+    paginator = Paginator(posts, 6)  # 6 posts per page
+    
+    # Get the current page number from the request's GET parameter
+    page_number = request.GET.get('page')
+    
+    # Get the posts for the current page
+    page_obj = paginator.get_page(page_number)
+    
+    # Return the response with the paginated posts
+    return render(request, 'blog/index.html', {'page_obj': page_obj})
