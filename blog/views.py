aed40aef476f5f1691293791e190d98faebc1dee
Abhishek Mahida <abhishekmahida56@gmail.com>
2024-12-13 17:03:58 +0000

    change to class based view

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,10 @@
-from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.views import generic
 from .models import PostBlog
 
-def fetchblog(request):
+class fetchblog(generic.ListView):
     # Fetch posts that are not drafts
-    posts = PostBlog.objects.filter(status=1)
+    queryset = PostBlog.objects.filter(status=1)
+    template_name = "blog/index.html"
+    paginate_by = 6
     
-    # Set pagination: 6 posts per page (simulate paginate_by)
-    paginator = Paginator(posts, 6)  # 6 posts per page
-    
-    # Get the current page number from the request's GET parameter
-    page_number = request.GET.get('page')
-    
-    # Get the posts for the current page
-    page_obj = paginator.get_page(page_number)
-    
-    # Return the response with the paginated posts
-    return render(request, 'blog/index.html', {'page_obj': page_obj})
