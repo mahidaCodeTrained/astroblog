6e6f3909e6c5a89ff5607d961e5357120445a2c5
Abhishek Mahida <abhishekmahida56@gmail.com>
2024-12-13 23:10:34 +0000

    add new view for detailed posts when clicked on the landing page

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import PostBlog
 
@@ -7,3 +7,26 @@ class FetchBlog(generic.ListView):
     template_name = "blog/index.html"
     paginate_by = 6  # 6 posts per page
 
+
+def detailed_posts(request, slug):
+    """
+    Display an individual :model:`blog.PostBlog`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.PostBlog`.
+
+    **Template:**
+
+    :template:`blog/detailed_posts.html`
+    """
+
+    queryset = PostBlog.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "blog/detailed_posts.html",
+        {"post": post},
+    )
