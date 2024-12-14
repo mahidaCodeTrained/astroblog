e31e3632bddfad96bc625e00f57954bd7145e2e9
Abhishek Mahida <abhishekmahida56@gmail.com>
2024-12-14 02:00:38 +0000

    create about models and views for template to showcase database entries.

--- about/views.py
+++ about/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
-from django.views import generic
+from .models import About
+
 
 def about(request):
-    return render(request, 'about/about.html')  
+    """
+    Renders the About page
+    """
+    about = About.objects.all().order_by('-updated_on').first()
+
+    return render(
+        request,
+        "about/about.html",
+        {"about": about},
+    )
