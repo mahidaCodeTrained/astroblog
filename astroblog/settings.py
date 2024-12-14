41ffc25a177b3d2fb54e809fea48528955fbc43f
Abhishek Mahida <abhishekmahida56@gmail.com>
2024-12-14 01:10:44 +0000

    testing new styles to templates with custom css and some bootstrap

--- astroblog/settings.py
+++ astroblog/settings.py
@@ -31,7 +31,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-mahidacodetra-astroblog-b7anplv6oaj.ws.codeinstitute-ide.net',
     '.herokuapp.com'
