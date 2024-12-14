8a33733b564a597f2efc056deabc4a8ab56d4db5
Abhishek Mahida <abhishekmahida56@gmail.com>
2024-12-14 20:54:21 +0000

    update urls.py to include create_post

--- blog/urls.py
+++ blog/urls.py
@@ -3,9 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.FetchBlog.as_view(), name='blog-home'),
+    path('create_post/', views.create_post, name='create_post'),
     path('<slug:slug>/', views.detailed_posts, name='detailed_posts'),
     path('<slug:slug>/edit_comment/<int:comment_id>',
          views.edits, name='edits'),
     path('<slug:slug>/delete_comment/<int:comment_id>',
-         views.deletion, name='deletion')
+         views.deletion, name='delete_comment'),
+    path('<slug:slug>/save_post/', views.save_post, name='save_post'),
+    path('<slug:slug>/unsave_post/', views.unsave_post, name='unsave_post'),
 ]
