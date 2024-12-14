1a922c6f827de828d03d9397ead52bf50a3fc9fc
Abhishek Mahida <abhishekmahida56@gmail.com>
2024-12-14 18:37:50 +0000

    new url paths upon comment and deletion

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.FetchBlog.as_view(), name='blog-home'),
     path('<slug:slug>/', views.detailed_posts, name='detailed_posts'),
+    path('<slug:slug>/edit_comment/<int:comment_id>',
+         views.edits, name='edits'),
+    path('<slug:slug>/delete_comment/<int:comment_id>',
+         views.deletion, name='deletion')
 ]
