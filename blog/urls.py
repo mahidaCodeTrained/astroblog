938ffc8c87a30f53b220b9a2adb0d49e94d1b6a5
Abhishek Mahida <abhishekmahida56@gmail.com>
2024-12-14 22:10:30 +0000

    include the script tags at the end of the html to ensure summernote editor is adjusting.

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.FetchBlog.as_view(), name='blog-home'),
     path('create_post/', views.create_post, name='create_post'),
+    path('post/edit/<int:post_id>/', views.edit_post, name='edit_post'),
     path('<slug:slug>/', views.detailed_posts, name='detailed_posts'),
     path('<slug:slug>/edit_comment/<int:comment_id>',
          views.edits, name='edits'),
