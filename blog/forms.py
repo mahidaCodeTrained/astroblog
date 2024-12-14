e7fd52b778e19ffe545652ad25c2df7aa8eff7ea
Abhishek Mahida <abhishekmahida56@gmail.com>
2024-12-14 22:11:45 +0000

    imported summernoteWidget (subject to remove)

--- blog/forms.py
+++ blog/forms.py
@@ -1,10 +1,12 @@
 from django import forms
+from django_summernote.widgets import SummernoteWidget
 from .models import PostBlog, Comment
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = PostBlog
         fields = ['title', 'slug', 'content', 'featured_image', 'excerpt', 'status']
+        
 
 class Commenting(forms.ModelForm):
     class Meta:
