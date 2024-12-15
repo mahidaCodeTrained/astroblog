fabffffb9f3941735f022991cfedcec639d0d981
Abhishek Mahida <abhishekmahida56@gmail.com>
2024-12-15 03:14:04 +0000

    debug back to true just to test

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Place the profile URL first to avoid conflict with the post detail URLs
     path('', views.FetchBlog.as_view(), name='blog-home'),
     path('create_post/', views.create_post, name='create_post'),
     path('post/edit/<slug:slug>/', views.edit_post, name='edit_post'),
@@ -11,5 +12,6 @@ urlpatterns = [
     path('<slug:slug>/save_post/', views.save_post, name='save_post'),
     path('<slug:slug>/unsave_post/', views.unsave_post, name='unsave_post'),
     path('<slug:slug>/delete/', views.delete_post, name='delete_post'),
-    
 ]
+
+
