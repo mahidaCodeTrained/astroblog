ff7e973bae865c5d39242b2dc11dd10231e78f49
Abhishek Mahida <abhishekmahida56@gmail.com>
2024-12-15 16:44:28 +0000

    ensure slug cant be edited

--- blog/forms.py
+++ blog/forms.py
@@ -6,6 +6,26 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = PostBlog
         fields = ['title', 'slug', 'content', 'featured_image', 'excerpt', 'status']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        # If editing an existing instance, remove the 'slug' field from the form
+        if self.instance and self.instance.pk:  # Check if the instance exists (i.e., we are editing)
+            del self.fields['slug']
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+
+        # Automatically generate the slug if it's not provided
+        if not instance.slug:
+            instance.slug = slugify(instance.title)
+
+        if commit:
+            instance.save()
+
+        return instance
+
         
 
 class Commenting(forms.ModelForm):
